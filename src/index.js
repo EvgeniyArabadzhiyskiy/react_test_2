205aa116f0186a06e5fe7798a2ab4d0df721b5dd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-05-26 13:02:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,31 +5,28 @@ import './index.css';
 import App from './App';
 
 
-// const root = ReactDOM.createRoot(document.querySelector("#root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  
+  <React.StrictMode>
     <App />
-  
+  </React.StrictMode>
 );
 
 
 
 
-
-// $ npm install modern-normalize
-// $ npm i prop-types
-// $ npm install classnames
-// $ npm install sass
-// $ npm i shortid
-// $ npm i axios
+// npx create-react-app .    //------------- install React App -------------------
 
 
+//  npm install modern-normalize
+//  npm i prop-types
+//  npm install classnames
+//  npm install sass
+//  npm i shortid
+//  npm i axios
 
-// $ npm install prop-types modern-normalize classnames sass shortid axios
+//  npm install --save react-toastify
+
+
+
+//  npm install prop-types modern-normalize classnames sass shortid axios
